c0496bc1d19cffa273e142b75eb8e9ae5d39869c
WailBenoulha <wailbnlh@gmail.com>
2024-11-07 23:40:49 -1100

    render config

--- app/app/settings.py
+++ app/app/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@&*cyaz6a=)gl%u49b3^-&49dr^s&zah)3xz73h*95ord+kyoy'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -112,6 +113,11 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+database_url =os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
+
+# postgresql://todo_eiqr_user:fSpHrhuj8uXLcZYQ9JKD70z58wGBc5FS@dpg-csmtvfggph6c73fptb0g-a.oregon-postgres.render.com/todo_eiqr
 
 
 # Password validation
